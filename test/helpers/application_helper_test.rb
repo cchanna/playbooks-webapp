39035dee1d49af5b73e94049a4ea520ffb0847c5
Cerisa <cerisa@strangerelics.com>
2015-06-05 16:39:02 -0700

    Fix link/title tests

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHlperTest < ActionView::TestCase
 	test "full title helper" do
-		assert_equal full_title, 				 "Ruby on Rails Tutorial Sample App"
-		assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+		assert_equal full_title, 				 "Playbooks Webapp"
+		assert_equal full_title("Help"), "Help | Playbooks Webapp"
 	end
 end
