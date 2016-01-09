3f261401fdc108c41276d028995761dab9027a11
Cerisa <cerisa@strangerelics.com>
2016-01-08 23:13:36 -0800

    added simple model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-root 'welcome#index'
+root 'characters#show', defaults: {id: 1}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
