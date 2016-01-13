d127439a0b9742f46f5453192cb43d7d9f791ffd
Cerisa <cerisa@strangerelics.com>
2016-01-12 17:43:08 -0800

    Ajax transitions This took way more work than it should have. But now, clicking on internal links results in a nice fade-out/fade-in transition, and the browser back button will have the same result (if appropriate).

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,23 @@
 class CharactersController < ApplicationController
   def show
     @character = Character.find(params[:id])
-    if (params[:partial])
-      render partial: "#{params[:partial]}", locals: {name: @character.name}
+
+    respond_to do |format|
+      format.html {render :text => "", :layout => true}
+      if (params[:partial])
+        format.js {render partial: "#{params[:partial]}", locals: {name: @character.name}}
+      else
+        format.js
+      end
     end
   end
 
   def new
     @character = Character.new
+    respond_to do |format|
+      format.html {render :text => "", :layout => true}
+      format.js
+    end
   end
 
   def create
@@ -31,5 +41,4 @@ class CharactersController < ApplicationController
     def character_params
       params.require(:character).permit(:name, :archetype_id)
     end
-    # @character = Character.create()
 end
