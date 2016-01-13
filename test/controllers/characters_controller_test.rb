eb0795e888c4e1a301cc112f257a4f6209ecd84c
Cerisa <cerisa@strangerelics.com>
2016-01-12 18:11:38 -0800

    Test updates Unfortunately, rspec doesn't use javascript, which means that I can no longer test if the views are rendering properly. I should probably set up Capybara or something to fill this gap.

--- test/controllers/characters_controller_test.rb
+++ test/controllers/characters_controller_test.rb
@@ -23,37 +23,8 @@ class CharactersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:character)
   end
 
-  test "name renders" do
-    show @claire
-    assert_select 'div#name', @claire.name
-  end
-
-  test "archetype renders" do
-    show @claire
-    assert_select 'div#archetype', "The " + @claire.archetype.name
-  end
-
   test "should show new character page" do
     get :new
     assert_response :success
-  end
-
-  test "should show setting symbol" do
-    setting_symbol @claire
-    assert_response :success
-    assert_not_nil assigns(:character)
-  end
-
-  test "setting symbol renders" do
-    setting_symbol @claire
-    assert_select '#symbol', @claire.archetype.setting_symbol
-  end
-
-  test "setting symbol examples render" do
-    setting_symbol @claire
-    example1 = @claire.archetype.setting_symbol_example1
-    example2 = @claire.archetype.setting_symbol_example2
-    example3 = @claire.archetype.setting_symbol_example3
-    assert_select '#examples', "(#{example1},#{example2},#{example3})"
   end
 end
