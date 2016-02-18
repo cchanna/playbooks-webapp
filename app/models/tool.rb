fcbe51038be3c5f8b9bc21f7293287dbf18df2fb
Cerisa <cerisa@strangerelics.com>
2016-02-18 13:18:13 -0800

    Changed tool "replaced_by" to "replaces"

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,7 +2,7 @@ class Tool < ActiveRecord::Base
   belongs_to :def_tool
   belongs_to :character
   belongs_to :improvement
-  has_one :replaced_by, class_name: "Tool",
+  has_one :replacement, class_name: "Tool",
                         foreign_key: "replaces"
   belongs_to :replaces, class_name: "Tool"
 end
