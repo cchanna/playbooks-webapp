c7acdfa25ab265ad67608514af738f85cd12793a
Cerisa <cerisa@strangerelics.com>
2016-02-18 13:12:52 -0800

    Added other-archetype move improvements

--- app/models/improvement.rb
+++ app/models/improvement.rb
@@ -1,9 +1,9 @@
 class Improvement < ActiveRecord::Base
   belongs_to :character
   belongs_to :def_improvement
-  has_one :stat_change
-  has_one :move
-  has_one :tool
-  has_one :vow
-  has_one :gift
+  has_one :stat_change, dependent: :destroy
+  has_one :move, dependent: :destroy
+  has_one :tool, dependent: :destroy
+  has_one :vow, dependent: :destroy
+  has_one :gift, dependent: :destroy
 end
